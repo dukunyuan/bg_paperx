0c5e13889d05099eda8960652337298e6f0b0cff
dukunyuan <dukunyuan2009@163.com>
2020-06-30 07:54:01 +0800

    头尾

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    header
+    <!--logo-->
+    <el-img src='static/img_three/h.jpg'></el-img>
+    <!--登陆-->
+    <el-button type="text" @click="loginDialog = true">登陆</el-button>
+    <!--登陆dialog-->
+  
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'homeHeader',
+    data() {
+      return {
+        loginDialog: false
+      }
+    },
+      methods: {
+  
+      }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/Order.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>订单</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'order'
+  }
+</script>
+
+<style>
+</style>
