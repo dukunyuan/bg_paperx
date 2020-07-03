903554ec811b78061cc23cbf5c2ffbb28b3c28fc
dukunyuan <dukunyuan2009@163.com>
2020-07-03 17:37:43 +0800

    登陆

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -6,20 +6,41 @@
     <!--登陆-->
     <el-button type="text" @click="loginDialog = true">登陆</el-button>
     <!--登陆dialog-->
-  
+    <el-dialog title="用户登录" :visible.sync="loginDialog" width="320px">
+     <el-form ref="form" :model="form" label-width="60px" >
+       <el-form-item label="用户名">
+         <el-input v-model="form.name" ></el-input>
+       </el-form-item>
+       <el-form-item label="密码">
+         <el-input v-model="form.password" show-password ></el-input>
+       </el-form-item>
+       <el-form-item label="验证码">
+         <Verify type="compute" figure="10" arith="0"></Verify>
+       </el-form-item>
+     </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+  import Verify from 'vue2-verify'
   export default {
     name: 'homeHeader',
+    components: {
+                Verify
+            },
     data() {
       return {
-        loginDialog: false
+        loginDialog: false,
+        //表单
+         form: {
+                  name: '',
+                 password: ''
+                }
       }
     },
       methods: {
-  
+
       }
   }
 </script>
