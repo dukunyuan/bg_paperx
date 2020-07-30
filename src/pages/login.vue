ccb99600d45d32b25d02f4b572d6b287695b4795
dukunyuan <dukunyuan2009@163.com>
2020-07-30 08:10:11 +0800

    给我上

---
+++ src/pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-tabs type="border-card">
+    <el-tab-pane label="注册">用户管理</el-tab-pane>
+    <el-tab-pane label="登录">配置管理</el-tab-pane>
+    <el-tab-pane label="找回密码">角色管理</el-tab-pane>
+  </el-tabs
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
