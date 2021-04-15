2631691db72fb12b241903137f63de430e756ecf
dukunyuan <dukunyuan2009@163.com>
2021-04-15 15:36:30 +0800

    编辑团队

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <h1>footer</h1>
+  <div class="footer">
+    <!--地址、联系方式、二维码-->
+    <div>
+      <!--地址-->
+      <div>
+        <h3>办公地址</h3>
+        <!--地图-->
+        <div></div>
+        <!--文字描述-->
+        <div></div>
+      </div>
+      <!--联系方式-->
+      <div></div>
+      <!--二维码-->
+      <div>备案信息123456789</div>
+    </div>
+    <!--备案信息-->
+    <div></div>
   </div>
 </template>
 
